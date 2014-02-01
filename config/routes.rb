d8b330ec040051baefe7a3d69d6ec7787d9308aa
unknown <admin1@VIT-WINL05C-12.wdc.vidyalankarlive.com>
2014-02-01 16:13:09 +0530

    Finish user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Project1::Application.routes.draw do
+  resources :users
+
+  resources :usernames
+
+  get "say/hello"
+  get "say/goodbye"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ test/helpers/usernames_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class UsernamesHelperTest < ActionView::TestCase
+end
